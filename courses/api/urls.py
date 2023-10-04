63c82d435fa8ce9ae0c3c8f82d697b4a1d065ec1
Mahmoud Zaytoni <mahmoudzaytoni2002@gmail.com>
2023-10-04 10:07:28 +0300

    Create another better way to enroll api using ViewSet

--- courses/api/urls.py
+++ courses/api/urls.py
@@ -1,5 +1,10 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 from . import views
+
+
+router = routers.DefaultRouter()
+router.register('courses', views.CourseViewSet)
 
 app_name = 'courses'
 
@@ -10,7 +15,8 @@ urlpatterns = [
      path('subjects/<pk>/',
           views.SubjectDetailView.as_view(),
           name='subject_detail'),
-     path('courses/<pk>/enroll/',
-          views.CourseEnrollView.as_view(),
-          name='course_enroll'),
+     # path('courses/<pk>/enroll/',
+     #      views.CourseEnrollView.as_view(),
+     #      name='course_enroll'),
+     path('', include(router.urls)),
 ]
--- courses/api/views.py
+++ courses/api/views.py
@@ -3,9 +3,11 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import action
 from rest_framework import generics
+from rest_framework import viewsets
 from courses.models import Subject, Course
-from courses.api.serializers import SubjectSerializer
+from courses.api.serializers import SubjectSerializer, CourseSerializer
 
 class SubjectListView(generics.ListAPIView):
     queryset = Subject.objects.all()
@@ -21,5 +23,18 @@ class CourseEnrollView(APIView):
 
     def post(self, request, pk, format=None):
         course = get_object_or_404(Course, pk=pk)
+        course.students.add(request.user)
+        return Response({'enrolled': True})
+
+class CourseViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+    @action(detail=True,
+            methods=['post'],
+            authentication_classes=[BasicAuthentication],
+            permission_classes=[IsAuthenticated])
+    def enroll(self, request, *args, **kwargs):
+        course = self.get_object()
         course.students.add(request.user)
         return Response({'enrolled': True})
