9be1e2d341c1ea2e69f2a719ef7956c753ea0197
Mahmoud Zaytoni <mahmoudzaytoni2002@gmail.com>
2024-05-03 15:18:40 +0300

    Disable Pagination

--- NeuraLearn/settings/base.py
+++ NeuraLearn/settings/base.py
@@ -161,7 +161,7 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
-    'DEFAULT_PAGINATION_CLASS': 'NeuraLearn.pagination.LimitOffsetPagination',
+    # 'DEFAULT_PAGINATION_CLASS': 'NeuraLearn.pagination.LimitOffsetPagination',
 }
 
 SIMPLE_JWT = {
