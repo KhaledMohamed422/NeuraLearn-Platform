813d0f5e3596a1117d700ff4dd3b9e270337d5f4
khaledmf <khaledmf517@gmail.com>
2024-06-18 16:39:18 +0300

    Update on courses/permissions.py

--- courses/permissions.py
+++ courses/permissions.py
@@ -8,5 +8,5 @@ class IsModuleOwnerPermission(DjangoModelPermissions, BasePermission):
 class IsInstructorPermission(BasePermission):
     def has_permission(self, request, view):
         print(request.user)
-        print(request.user.groups.filter(name='Instructors').exists())
+        print(request.user.groups.filter(name='Instructors'))
         return request.user.groups.filter(name='Instructors').exists()
