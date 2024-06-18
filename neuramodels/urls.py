95ed48e8cb16fe9b4bbf91c35d4eaadca5fa10b1
khaledmf <khaledmf517@gmail.com>
2024-06-18 22:47:25 +0300

    Update on neuramodels/urls.py

--- neuramodels/urls.py
+++ neuramodels/urls.py
@@ -5,6 +5,6 @@ app_name = 'neuramodels'
 
 urlpatterns = [
     path('module/<slug:slug>/transcripts/', views.module_get_transcripts, name='module-get-transcripts'),
-    path('video/<int:id>/transcript/', views.VideoGetTranscripts.as_view, name='video-get-transcript'),
+    path('video/<int:id>/transcript/', views.VideoGetTranscripts.as_view(), name='video-get-transcript'),
     path('summarize/', views.Summarizer.as_view(), name='summarizer'),
 ]
