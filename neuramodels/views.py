1c1eef3100c21f777dfbfac8e8ede6b9d6a3e26e
khaledmf <khaledmf517@gmail.com>
2024-06-19 04:57:46 +0300

    Update on ChatBotAPIView view for neuramodels

--- neuramodels/views.py
+++ neuramodels/views.py
@@ -147,7 +147,7 @@ class QuestionGeneration(APIView):
 @extend_schema(
     tags=['Question Answer (Chatbot)'],
     request=ChatBotRequestSerializer,
-    responses={200: ChatBotResponseSerializer(many=True)}
+    responses={200: dict}
 )
 class ChatBotAPIView(APIView):
     def post(self, request):
@@ -157,7 +157,7 @@ class ChatBotAPIView(APIView):
             question = serializer.validated_data['question']
             chat_history = serializer.validated_data['chat_history']
             k = serializer.validated_data['k']
-            
+
             # Send data to the model server
             payload = {
                 "context": context,
