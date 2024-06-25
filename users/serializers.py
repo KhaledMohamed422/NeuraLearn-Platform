9c57e14dabdddb475e0e01d67ec155c646acde5d
khaledmf <khaledmf517@gmail.com>
2024-06-25 11:22:38 +0300

    Update on user info

--- users/serializers.py
+++ users/serializers.py
@@ -1,8 +1,20 @@
-from djoser.serializers import UserCreateSerializer
+from django.contrib.auth.models import Group
+from djoser.serializers import UserCreateSerializer as BaseUserCreateSerializer
 from django.contrib.auth import get_user_model
+
 User = get_user_model()
 
-class UserCreateSerializer(UserCreateSerializer):
-    class Meta(UserCreateSerializer.Meta):
+class UserCreateSerializer(BaseUserCreateSerializer):
+    class Meta(BaseUserCreateSerializer.Meta):
         model = User
-        fields = ('id', 'email', 'first_name', 'last_name', 'password')
+        fields = ('id', 'email', 'first_name', 'last_name', 'is_student', 'is_instructor', 'password')
+
+    def create(self, validated_data):
+        is_instructor = validated_data.pop('is_instructor', False)
+        user = super().create(validated_data)
+        
+        if is_instructor:
+            instructor_group, created = Group.objects.get_or_create(name='instructor')
+            user.groups.add(instructor_group)
+        
+        return user
