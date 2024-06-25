a5a485a37bf9cd83b6765ce3ea5393b5fc90d052
khaledmf <khaledmf517@gmail.com>
2024-06-26 02:49:44 +0300

    Update on endpoint user

--- users/serializers.py
+++ users/serializers.py
@@ -10,12 +10,24 @@ class UserCreateSerializer(BaseUserCreateSerializer):
         fields = ('id', 'email', 'first_name', 'last_name', 'is_student', 'is_instructor', 'password')
 
     def create(self, validated_data):
+        # Extract the custom fields
         is_instructor = validated_data.pop('is_instructor', False)
+        is_student = validated_data.pop('is_student', False)
+
+        # Create the user
         user = super().create(validated_data)
-        
+        user.is_instructor = is_instructor
+        user.is_student = is_student
+        user.save()
+
+        # Add the user to the 'instructor' group if is_instructor is True
         if is_instructor:
-            instructor_group, created = Group.objects.get_or_create(name='instructor')
-            user.groups.add(instructor_group)
-            user.save()
+            try:
+                instructor_group, created = Group.objects.get_or_create(name='instructor')
+                user.groups.add(instructor_group)
+                user.save()
+                print(f"User {user.email} added to 'instructor' group.")
+            except Exception as e:
+                print(f"Failed to add user {user.email} to 'instructor' group: {e}")
 
         return user
