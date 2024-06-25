155623fd1b220ca3612e6f82b0d942d64a24a92f
khaledmf <khaledmf517@gmail.com>
2024-06-25 06:26:12 +0300

    Update on GetTranscriptAPIView view for neuramodel

--- neuramodels/urls.py
+++ neuramodels/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
     path('module/<slug:slug>/transcripts/', views.module_get_transcripts, name='module-get-transcripts'),
     path('module/<slug:slug>/transcripts_general/', views.GetTranscript.as_view(), name='module-get-transcripts_general'),
     path('summarize/', views.Summarizer.as_view(), name='summarizer'),
-    path('generate-questions/', views.QuestionGeneration.as_view(), name='question_generation'),
+    # path('generate-questions/', views.QuestionGeneration.as_view(), name='question_generation'),
     path('chatbot/', views.ChatBotAPIView.as_view(), name='chatbot'),
-
 ]
